c7b1e26e62c27f11cd34fa1deb9a18e76fd06264
Davyusow <dayvsonf27@gmail.com>
2026-01-18 15:54:47 -0300

    Classe do Jogador inicial

---
+++ src/model/player.py
@@ -0,0 +1,20 @@
+class Jogador:
+    # variáveis do jogador
+    vida: int = 10
+    vida_max: int = 10
+    nivel: int = 1
+    dano: int = 3
+    nome: str = "jogador"
+    exp : int = 0
+    prox_nivel : int = 10
+
+    def __init__(self, nome: str = "jogador"):
+        self.nome = nome
+
+    def atacar(self) -> None:
+        print(f"{self.nome} ataca, causando {self.dano} de dano!")
+
+    def subir_de_nivel(self) -> None:
+        self.nivel+=1
+        self.vida_max *= int(1.2)
+        self.vida = self.vida_max # Cura o jogador após subir de nível
