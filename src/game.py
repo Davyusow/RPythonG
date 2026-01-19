9cb0f999bc3321ae8e1e804fda1ac8f68e3ca88d
Davyusow <dayvsonf27@gmail.com>
2026-01-19 03:48:01 -0300

    Atualização do init do inimigo, agora pode ter o nível passado diretamente

--- src/game.py
+++ src/game.py
@@ -3,7 +3,7 @@ from src.model.player import Jogador
 from src.view.status import status_batalha
 
 player: Jogador = Jogador(input("Digite o Nome do Jogador!: "))
-enemy: Inimigo = Inimigo("Orc")
+enemy: Inimigo = Inimigo("Orc",4)
 
 player.atacar(enemy)
 status_batalha(player, enemy)
--- src/model/enemy.py
+++ src/model/enemy.py
@@ -2,11 +2,14 @@ class Inimigo:
     vida: int = 10
     vida_max: int = 10
     nivel: int = 1
-    dano: int = 3
+    dano: int = 2
     nome: str
 
-    def __init__(self, nome: str = "Inimigo"):
+    def __init__(self, nome: str = "Inimigo", nivel : int = 1):
         self.nome = nome
+        self.vida_max += int(nivel*1.5)
+        self.vida = self.vida_max
+        self.dano += int(nivel*1.5)
 
     def atacar(self) -> None:
         print(f"{self.nome} ataca, causando {self.dano} de dano!")
